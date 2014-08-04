b038db2c173737f0a3a3be9241ad07e6c209283a
derpston <derpston@sleepygeek.org>
2014-08-04 21:53:18 +0100

    Added example fruitfly application.

---
+++ example/mod_example.py
@@ -0,0 +1,19 @@
+"""
+A basic fruitfly module. Sends an event once per second, and receives the same event.
+"""
+
+import fruitfly
+
+class example(fruitfly.Module):
+    def init(self):
+        self.onesecond()
+
+    @fruitfly.repeat(1)
+    def onesecond(self):
+        self._logger.info("Sending an event")
+        self.send_event("foo.omg", ['test'])
+
+    @fruitfly.event('foo.*')
+    def foohandler(self, event, payload):
+       self._logger.info("event handler for foo got %s, %s", event, repr(payload))
+
